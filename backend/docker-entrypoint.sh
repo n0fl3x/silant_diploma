00f9b8a90b75307a72acfb56970c8d5fb65a2dcb
Roman Nevzorov <pomkanevzorov@gmail.com>
2026-03-04 00:56:01 +0500

    Depl fixes.

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-
-echo "Starting..."
-
-echo "Creating migrations..."
 python manage.py makemigrations --noinput
-
-echo "Applying migrations..."
 python manage.py migrate --noinput
-
-echo "Running custom setup commands..."
 python manage.py setup-groups
 python manage.py god-create
 python manage.py db-import-machines
 python manage.py db-import-maintenance
 python manage.py db-import-claims
-
-echo "Starting Django server..."
 exec python manage.py runserver 0.0.0.0:8000
